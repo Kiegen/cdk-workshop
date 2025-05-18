a7d3cd4f491c6089659e7c03bbe5fffd60513ec2
Kiegen Chinna <kiegenchinna@MacBookPro.station>
2025-05-18 19:52:26 +0200

    Add deploy stage to pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,13 +1,14 @@
 import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { WorkshopPipelineStage } from './pipeline-stage';
 
 
 export class WorkshopPipelineStack extends Stack {
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-
+        
         const pipeline = new CodePipeline(this, "Pipeline", {
             pipelineName: "WorkshopPipeline",
             synth: new ShellStep("Synth", {
@@ -21,5 +22,8 @@ export class WorkshopPipelineStack extends Stack {
                 ],
             }),
         })
+
+        const deploy = new WorkshopPipelineStage(this, "Deploy")
+        
     }
 }

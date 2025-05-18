5877623af122e1ff4b68cca2d64302ab9fb47a31
Kiegen Chinna <kiegenchinna@MacBookPro.station>
2025-05-18 20:35:46 +0200

    add validation

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,6 +1,6 @@
 import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ShellStep, CodeBuildStep } from 'aws-cdk-lib/pipelines';
 import { WorkshopPipelineStage } from './pipeline-stage';
 
 
@@ -8,7 +8,7 @@ export class WorkshopPipelineStack extends Stack {
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-        
+
         const pipeline = new CodePipeline(this, "Pipeline", {
             pipelineName: "WorkshopPipeline",
             synth: new ShellStep("Synth", {
@@ -25,6 +25,30 @@ export class WorkshopPipelineStack extends Stack {
 
         const deploy = new WorkshopPipelineStage(this, "Deploy")
         const deployStage = pipeline.addStage(deploy);
-        
+
+        deployStage.addPost(
+            new CodeBuildStep('TestViewerEndpoint', {
+                projectName: 'TestViewerEndpoint',
+                envFromCfnOutputs: {
+                    ENDPOINT_URL:  deploy.hcViewerUrl,
+        },
+                commands: [
+                    'curl -Ssf $ENDPOINT_URL'
+                ]
+            }),
+
+            new CodeBuildStep('TestAPIGatewayEndpoint', {
+                projectName: 'TestAPIGatewayEndpoint',
+                envFromCfnOutputs: {
+                    ENDPOINT_URL: deploy.hcEndpoint,
+        },
+                commands: [
+                    'curl -Ssf $ENDPOINT_URL',
+                    'curl -Ssf $ENDPOINT_URL/hello',
+                    'curl -Ssf $ENDPOINT_URL/test'
+                ]
+            })
+        )
+
     }
 }

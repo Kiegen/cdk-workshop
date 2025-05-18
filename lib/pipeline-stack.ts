7411e49967290d1192fac8b3a249e64af0448e17
Kiegen Chinna <kiegenchinna@MacBookPro.station>
2025-05-18 18:59:18 +0200

    MESSAGE

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,14 +1,25 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
+import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import {Repository} from 'aws-cdk-lib/aws-codecommit';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+
 
 export class WorkshopPipelineStack extends Stack {
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const repo = new Repository(this, 'WorkshopRepo', {
-            repositoryName: 'WorkshopRepo'
-    });
-}
+        const pipeline = new CodePipeline(this, "Pipeline", {
+            pipelineName: "WorkshopPipeline",
+            synth: new ShellStep("Synth", {
+                input: CodePipelineSource.gitHub("Kiegen/cdk-workshop", "main", {
+                    authentication: SecretValue.secretsManager("github-token-cdk-workshop"),
+                }),
+                commands: [
+                    "npm ci",
+                    "npm run build",
+                    "npx cdk synth",
+                ],
+            }),
+        })
+    }
 }
